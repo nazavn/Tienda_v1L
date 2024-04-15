22c086312ed7256ae806af8a6e02e5145bc6835e
nazavn <126742695+nazavn@users.noreply.github.com>
2024-04-15 16:09:35 -0600

    cambios

--- src/main/java/com/tienda/controller/ReporteController.java
+++ src/main/java/com/tienda/controller/ReporteController.java
@@ -15,10 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- *
- * @author royam
- */
 @Controller
 @RequestMapping("/reportes")
 public class ReporteController {
