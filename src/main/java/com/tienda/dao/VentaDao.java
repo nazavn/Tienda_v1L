26fcd4e002b1142d4346ebe9d593de99decce944
nazavn <126742695+nazavn@users.noreply.github.com>
2024-04-15 16:31:06 -0600

    .

--- src/main/java/com/tienda/dao/VentaDao.java
+++ src/main/java/com/tienda/dao/VentaDao.java
@@ -7,10 +7,7 @@ package com.tienda.dao;
 import com.tienda.domain.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- *
- * @author royam
- */
+
 public interface VentaDao extends JpaRepository <Venta, Long>{
     
 }
